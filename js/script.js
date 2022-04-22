a3da0657d1718d69fbdb7746dad801684de48b44
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-04-22 13:49:01 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ function answer() {
 	let height = parseFloat(document.getElementById('height').value)
 	let unit = document.getElementById('unit').value
 	//Calculates SA and volume
-	let surfaceArea = 5 * sideA * height + 1 / 2 * Math.sqrt(5(5 + 2 * Math.sqrt(5))) * sideA ** 2
-	let volume = 
+	let surfaceArea = 5 * sideA * height + 1 / 2 * Math.sqrt(5(5 + 2 * Math.sqrt(5))) * (sideA ** 22)
+	let volume = 1 / 4 * Math.sqrt(5(5 + 2 * Math.sqrt(5))) * (sideA ** 2) * height
 	//Answers
-	document.getElementById('surface-area').innerHTML = 'The surface area of the cone is ' + surfaceArea.toFixed(2) + " " + unit + "²"
-	document.getElementById('volume').innerHTML = 'The volume of the cone is ' + volume.toFixed(2) + " " + unit + "³" 
+	document.getElementById('surface-area').innerHTML = 'The surface area of the pentagonal prism is ' + surfaceArea.toFixed(2) + " " + unit + "²"
+	document.getElementById('volume').innerHTML = 'The volume of the pentagonal prism is ' + volume.toFixed(2) + " " + unit + "³" 
 }
