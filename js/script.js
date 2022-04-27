9faa22a68178db4570831d7202bca73abd427313
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-04-27 15:52:43 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
 'use strict';
 function answer() {
 	// Declares variables 
-	let sideA = parseFloat(document.getElementById('side-a').value)
-	let height = parseFloat(document.getElementById('height').value)
+	let sideA = document.getElementById('side-a').value
+	let height = document.getElementById('height').value
 	let units = document.getElementById('unit').value
 	//Calculates SA and volume
 	let surfaceArea = 5 * sideA * height + 1 / 2 * Math.sqrt(5 * (5 + 2 * Math.sqrt(5))) * (sideA ** 2)
@@ -16,16 +16,20 @@ function answer() {
 	
 	
 	// If statement for unit of measurement (Van helped a lot)
-	if (units === "mm" || units === "cm" || units === "dm" || units === "m" ||units === "dam" || units === "hm" || units === "km") {
-		//Outputs SA and volume of a Pentagonal Prism
-		document.getElementById('surface-area').innerHTML = 'The surface area of the pentagonal prism is ' + surfaceArea.toFixed(2) + " " + units + "²";
-		document.getElementById('volume').innerHTML = 'The volume of the pentagonal prism is ' + volume.toFixed(2) + " " + units + "³";
-		document.getElementById('error').innerHTML = "";
-	} else {
-		//Error message if unit of measurement is invalid
-		document.getElementById('error').innerHTML = "The unit of measurement you chose is not valid. Please enter one of the following: mm, cm, dm, m, dam, hm or km";
+	if (sideA != "" && height != " " && units != "")  {
+		if (units === "mm" || units === "cm" || units === "dm" || units === "m" ||units === "dam" || units === "hm" || units === "km") {
+			document.getElementById('surface-area').innerHTML = 'The surface area of the pentagonal prism is ' + surfaceArea.toFixed(2) + " " + units + "²";
+			document.getElementById('volume').innerHTML = 'The volume of the pentagonal prism is ' + volume.toFixed(2) + " " + units + "³";
+			document.getElementById('error').innerHTML = "";
+		} else {
+			//Error message if unit of measurement is invalid
+			document.getElementById('error').innerHTML = "The unit of measurement you chose is not valid. Please enter one of the following: mm, cm, dm, m, dam, hm or km";
+			document.getElementById('volume').innerHTML = ""
+			document.getElementById('surface-area').innerHTML = ""
+		}
+	}	else {
+		document.getElementById('error').innerHTML = "You need to fill in all the text fields to determine the surface area and volume.";
 		document.getElementById('volume').innerHTML = ""
 		document.getElementById('surface-area').innerHTML = ""
-		
-	} 
+	}
 }
